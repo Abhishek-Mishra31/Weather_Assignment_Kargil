1de53f7376dcbdbeb1ed12d962eccda82bd1edc9
Abhishek Mishra <Abhishekbelaganj0609@gmail.com>
2025-10-03 14:28:18 +0530

    added: axios

--- src/Components/Section.jsx
+++ src/Components/Section.jsx
@@ -1,7 +1,23 @@
 import { React, useState } from "react";
+import axios from "axios";
+
+const apiClint = axios.create({
+  baseURL: "http://api.weatherapi.com/v1",
+  timeout: 5000,
+});
+
+apiClint.interceptors.request.use(null, async (error) => {
+  const config = error.config;
+  if (!error.response && config && !config.__isRetryRequest) {
+    config.__isRetryRequest = true;
+    console.log("Retrying the request");
+    return apiClint(config);
+  }
+  return Promise.reject(error);
+});
 
 function Section() {
-  const [count, setCount] = React.useState(0);
+  // const [count, setCount] = React.useState(0);
   async function fetchWeather() {
     const city = document.getElementById("cityInput").value;
     const cityname = document.getElementById("city");
@@ -13,10 +29,13 @@ function Section() {
     }
 
     try {
-      const response = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`
-      );
-      const data = await response.json();
+      const response = await apiClint.get("/current.json", {
+        params: {
+          key: apiKey,
+          q: city,
+        },
+      });
+      const data = response.data;
       cityname.innerText = `City: ${data.location.name}`;
       temp.innerText = `Temperature: ${data.current.temp_c} °C`;
       cond.innerText = `Condition: ${data.current.condition.text}`;
@@ -30,15 +49,14 @@ function Section() {
       <div style={{ marginTop: "50px" }}>
         <h2>Counter App</h2>
         <p>Simple Counter App using useState Hook</p>
-        <h3>Count: {count}</h3>
-        <button
+        <h3>Count: </h3>
+        {/* <button
           onClick={(e) => {
-            e.preventDefault();
-            incrementCount();
+            e.preventDefault(); incrementCount();
           }}
         >
           click me to increase counter
-        </button>
+        </button> */}
       </div>
 
       <div style={{ marginTop: "80px" }}>
